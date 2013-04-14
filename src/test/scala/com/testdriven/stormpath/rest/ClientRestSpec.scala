58fb3edaab068b353c76b1e1f156a9170d0dbeee
David Vydra <david@vydra.net>
2013-04-13 23:39:51 -0700

    check for cookie header

--- src/test/scala/com/testdriven/stormpath/rest/ClientRestSpec.scala
+++ src/test/scala/com/testdriven/stormpath/rest/ClientRestSpec.scala
@@ -11,12 +11,12 @@ import com.testdriven.stormpath.fixtures.TestConfig
 class ClientRestSpec extends MyRestAssuredConfig with FlatSpec with ShouldMatchers {
 
   "Current tenant" should "be correct" in {
-     given().
-     expect().
+     given().   //log().all().
+     expect().   //log().all().
           statusCode(200).
+          header("Set-Cookie", startsWith("rememberMe=deleteMe; Path=/; Max-Age=0; Expires=")).
           body("name", equalTo( TestConfig.tenantName )).
      when().
        get("/tenants/current")
   }
-
 }
